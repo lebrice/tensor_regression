13909ea9b3d5b609d30061654398aafe890b2c70
Fabrice Normandin <normandf@mila.quebec>
2024-08-05 15:41:30 -0400

    Use a ndarrays regression fixture and numpy.random
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>

--- tensor_regression/tests/test_check.py
+++ tensor_regression/tests/test_check.py
@@ -1,12 +1,11 @@
 import contextlib
 import random
-import textwrap
 from pathlib import Path
 
 import numpy as np
 import pytest
 import torch
-import yaml
+from pytest_regressions.ndarrays_regression import NDArraysRegressionFixture
 
 from tensor_regression import TensorRegressionFixture
 
@@ -27,6 +26,10 @@ def device(request: pytest.FixtureRequest) -> torch.device:
 def seeded(seed: int, devices: list[torch.device] | None = None):
     random_state = random.getstate()
     np_random_state = np.random.get_state()
+    if devices is None:
+        devices = [
+            torch.device("cuda", index=i) for i in range(torch.cuda.device_count())
+        ]
     with torch.random.fork_rng(devices=devices):
         torch.manual_seed(seed)
         random.seed(seed)
@@ -48,28 +51,27 @@ def seed(request: pytest.FixtureRequest, device: torch.device):
 @pytest.mark.parametrize("label", [None, "some_label"], ids="label={}".format)
 def test_check_tensor(
     tensor_regression: TensorRegressionFixture,
-    seed: int,
     device: torch.device,
     monkeypatch: pytest.MonkeyPatch,
     tmp_path: Path,
     label: str | None,
+    ndarrays_regression: NDArraysRegressionFixture,
 ):
     # Make it so our tensor regression fixture operates within a temporary directory, instead of
     # the actual test data dir (next to this test).
     monkeypatch.setattr(tensor_regression, "original_datadir", tmp_path)
     monkeypatch.setattr(tensor_regression, "generate_missing_files", True)
 
-    gen = torch.Generator(device=device).manual_seed(seed)
-    x = torch.randn(3, 3, generator=gen, device=device)
+    x = torch.from_numpy(np.random.default_rng(seed=123).random(size=(3, 3))).to(
+        device=device
+    )
     tensor_regression.check(
         {"x": x}, additional_label=label, include_gpu_name_in_stats=False
     )
 
     # Check that stats were saved:
-    # todo: use a pytest_regression fixture for the generated stats instead of hard-coding them? Or
-    # would that become too confusing?
-
-    # TODO: A bit confusing here that the subfolder is part of the stats file, because this test is parametrized.
+    # TODO: It's a bit confusing that the file name include `label_{label}`, but that's because
+    # this test is parametrized.
     if label is None:
         stats_file = tmp_path / test_check_tensor.__name__ / f"label_{label}.yaml"
     else:
@@ -79,22 +81,7 @@ def test_check_tensor(
 
     assert stats_file.exists()
 
-    assert yaml.safe_load(stats_file.read_text()) == yaml.safe_load(
-        textwrap.dedent(
-            f"""\
-            x:
-              device: {device}
-              hash: 4237770438332455960
-              max: 1.573499083518982
-              mean: 0.41351261734962463
-              min: -1.6879240274429321
-              shape:
-              - 3
-              - 3
-              sum: 3.7216134071350098
-            """
-        )
-    )
+    ndarrays_regression.check({"x": x.detach().cpu().numpy()})
 
     # Check the saved tensor/array:
     array_file = stats_file.with_suffix(".npz")
